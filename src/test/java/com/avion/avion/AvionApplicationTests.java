bf2e95ee9eb7a57474dbd915d4480b5e2daaf947
MioraRatsiresy <miora.ratsiresy@gmail.com>
2022-12-20 09:30:37 +0300

    commit

---
+++ src/test/java/com/avion/avion/AvionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avion.avion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
